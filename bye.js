69d2824f7a76f1a7dffd3fb432c0afb142f47933
renniw <55645539+Magvel@users.noreply.github.com>
2020-06-25 18:04:06 +0530

    Add files via upload

---
+++ bye.js
@@ -0,0 +1,16 @@
+
+(function(window){
+
+	var byeSpeaker={};
+
+	byeSpeaker.speak=function(name){
+	console.log(speakWord + " " + name);
+	};
+
+	var speakWord = "Bye"
+
+	window.byeSpeaker=byeSpeaker;
+
+})(window);
+
+
